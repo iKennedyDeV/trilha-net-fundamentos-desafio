dc259af5339082135b821a32c3f4264fcf0083f1
Renan kenedy da silva <115326944+iKennedyDeV@users.noreply.github.com>
2023-12-28 22:36:24 -0300

    Update Estacionamento.cs

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -1,67 +1,64 @@
-namespace DesafioFundamentos.Models
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projeto_Estacionamento_C_.moldels
 {
     public class Estacionamento
     {
+        private List<string> Veiculos = new List<string>();
         private decimal precoInicial = 0;
-        private decimal precoPorHora = 0;
-        private List<string> veiculos = new List<string>();
+        private decimal precoHora = 0;
 
-        public Estacionamento(decimal precoInicial, decimal precoPorHora)
+        public Estacionamento(decimal inicial, decimal hora)
         {
-            this.precoInicial = precoInicial;
-            this.precoPorHora = precoPorHora;
-        }
+            this.precoInicial = inicial;
+            this.precoHora = hora;
 
+        }
         public void AdicionarVeiculo()
         {
-            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
-            // *IMPLEMENTE AQUI*
-            Console.WriteLine("Digite a placa do veículo para estacionar:");
+            Console.WriteLine("Digite a placa do veiculo para estacionar:  ex:ABC-1234: ");
+            string placaVeiculo = Console.ReadLine();
+            Veiculos.Add(placaVeiculo);
         }
 
         public void RemoverVeiculo()
         {
-            Console.WriteLine("Digite a placa do veículo para remover:");
-
-            // Pedir para o usuário digitar a placa e armazenar na variável placa
-            // *IMPLEMENTE AQUI*
+            Console.WriteLine("Digite a placa do veiculo para remover");
             string placa = "";
-
-            // Verifica se o veículo existe
-            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            placa = Console.ReadLine();
+            if (Veiculos.Any(placa => placa.ToUpper() == placa.ToUpper()))
             {
-                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+                Console.WriteLine("Digite a quantidade de horas que o veiuculo permaneceu estacionado:");
+                int tempoEstacionado = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Valor totalizado com as horas utilizada do serviço: {precoInicial + (precoHora * tempoEstacionado):c}");
 
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
-                int horas = 0;
-                decimal valorTotal = 0; 
-
-                // TODO: Remover a placa digitada da lista de veículos
-                // *IMPLEMENTE AQUI*
-
-                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
             }
             else
             {
-                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+                Console.WriteLine($"Placa{placa} não encontrada");
             }
-        }
+            Veiculos.Remove(placa);
 
-        public void ListarVeiculos()
+
+        }
+        public void ListarVeiculo()
         {
-            // Verifica se há veículos no estacionamento
-            if (veiculos.Any())
+            if (Veiculos.Any())
             {
-                Console.WriteLine("Os veículos estacionados são:");
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
+                Console.WriteLine($"Todos os Veiculos Estacionados:");
+                for (int i = 0; i < Veiculos.Count; i++)
+                {
+                    Console.WriteLine($"N°{i + 1} - {Veiculos[i]}");
+                }
+
+            }else{
+                Console.WriteLine("Não há veiculos estacionados");
             }
-            else
-            {
-                Console.WriteLine("Não há veículos estacionados.");
-            }
+
         }
+
     }
 }
